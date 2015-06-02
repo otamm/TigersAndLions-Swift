387362a41149a89962bf861270fecb25fb51a7c6
otamm <oivatom@gmail.com>
2015-06-02 18:57:30 -0300

    Randomized animations and tigers display

--- TigersAndLions/ViewController.swift
+++ TigersAndLions/ViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // elements linked from storyboard
     @IBOutlet weak var mainImageView: UIImageView! ;
     @IBOutlet weak var nameLabel: UILabel! ;
     @IBOutlet weak var ageLabel: UILabel! ;
     @IBOutlet weak var breedLabel: UILabel! ;
+    
+    // arrays, one for the tigers, other for the possible animations
+    
     var tigers:Array<Tiger> = []; // globally accessed, filled with content after ViewDidLoad()
-    var animations:Array<UIViewAnimationOptions> = [];
+    var animations:Array<UIViewAnimationOptions> = [
+        UIViewAnimationOptions.TransitionCrossDissolve,
+        UIViewAnimationOptions.TransitionCurlDown, UIViewAnimationOptions.TransitionFlipFromLeft,
+        UIViewAnimationOptions.TransitionFlipFromTop
+    ];
+    
+    // indexes
+    var tigerIndex:Int = 0;
+    var animationIndex:Int = 0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,9 +50,25 @@ class ViewController: UIViewController {
         yetAnotherTiger.breed = "3D Digital Animation";
         yetAnotherTiger.picture = UIImage(named: "tigress_by_jona.jpg");
         
+        // fill array with available tigers
         tigers.append(tiger);
         tigers.append(anotherTiger);
         tigers.append(yetAnotherTiger);
+        
+        // randomizes first tiger to be displayed;
+        tigerIndex = Int(arc4random_uniform(UInt32(tigers.count)));
+        
+        // without this line, the animation at index 0 would never be the first, the process was 75% randomized; now it is 100%.
+        animationIndex = Int(arc4random_uniform(UInt32(animations.count)));
+        
+        // displays tiger info when UIView loads;
+        nameLabel.text = tigers[tigerIndex].name;
+        ageLabel.text = "\(tigers[tigerIndex].age)";
+        breedLabel.text = tigers[tigerIndex].breed;
+        mainImageView.image = tigers[tigerIndex].picture;
+        
+        println("HI");
+        
         
     }
 
@@ -62,17 +90,32 @@ class ViewController: UIViewController {
         // options: A mask of options indicating how you want to perform the animations. For a list of valid constants, see UIViewAnimationOptions.
         // animations: A block object that contains the changes you want to make to the specified view. This block takes no parameters and has no return value. This parameter must not be NULL.
         // completion: A block object to be executed when the animation sequence ends. This block has no return value and takes a single Boolean argument that indicates whether or not the animations actually finished before the completion handler was called. If the duration of the animation is 0, this block is performed at the beginning of the next run loop cycle. This parameter may be NULL.
-        var tigerzzz:[Tiger] = tigers;
-        let randomIndex = Int(arc4random_uniform(UInt32(tigers.count)));
-        //tigerzzz.removeAtIndex()
-        let tiger = tigerzzz[randomIndex];
         
-        UIView.transitionWithView(self.view, duration: 1, options: UIViewAnimationOptions.TransitionCrossDissolve, animations:
+        var randomIndex = Int(arc4random_uniform(UInt32(tigers.count)));
+        // assures the random index to be different than the current.
+        while (tigerIndex == randomIndex)
+        {
+            randomIndex = Int(arc4random_uniform(UInt32(tigers.count)));
+        }
+        
+        tigerIndex = randomIndex;
+        
+        var randomIndex2 = Int(arc4random_uniform(UInt32(animations.count)));
+        while (animationIndex == randomIndex2)
+        {
+            randomIndex2 = Int(arc4random_uniform(UInt32(animations.count)));
+        }
+        // assures a different animation each time
+        animationIndex = randomIndex2;
+        
+        let tiger = tigers[randomIndex];
+        
+        UIView.transitionWithView(self.view, duration: 1, options: animations[animationIndex], animations:
             {
-                self.nameLabel.text = tiger.name;
-                self.ageLabel.text = "\(tiger.age)";
-                self.breedLabel.text = tiger.breed;
-                self.mainImageView.image = tiger.picture;
+                self.nameLabel.text = self.tigers[self.tigerIndex].name;
+                self.ageLabel.text = "\(self.tigers[self.tigerIndex].age)";
+                self.breedLabel.text = self.tigers[self.tigerIndex].breed;
+                self.mainImageView.image = self.tigers[self.tigerIndex].picture;
             }, completion: { (finished: Bool) -> () in } );
 
     }
