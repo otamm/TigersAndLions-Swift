038257966716a2b69f50f3e1d52780f431bd6eec
otamm <oivatom@gmail.com>
2015-06-02 01:13:52 -0300

    Array of tigers created

---
+++ TigersAndLions/Tiger.swift
@@ -0,0 +1,20 @@
+//
+//  Tiger.swift
+//  TigersAndLions
+//
+//  Created by Otavio Monteagudo on 5/28/15.
+//  Copyright (c) 2015 Otavio Monteagudo. All rights reserved.
+//
+
+import Foundation;
+import UIKit; // info will be placed inside a UIView
+
+struct Tiger {
+    
+    // info will be supplied when instantiated
+    var age = 0;
+    var name = "";
+    var breed = "";
+    var picture = UIImage(named:"");
+    
+}
---
+++ TigersAndLions/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  TigersAndLions
+//
+//  Created by Otavio Monteagudo on 5/28/15.
+//  Copyright (c) 2015 Otavio Monteagudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var tiger = Tiger(); //struct Tiger defined in Tiger.swift
+        tiger.age = 3;
+        tiger.name = "Hobbes";
+        tiger.breed = "Cotton, Made in China";
+        tiger.picture = UIImage(named:"hobbes.jpeg");
+        
+        nameLabel.text = tiger.name;
+        ageLabel.text = "\(tiger.age)";
+        breedLabel.text = tiger.breed;
+        mainImageView.image = tiger.picture;
+        
+        var anotherTiger = Tiger();
+        anotherTiger.age = 0;
+        anotherTiger.name = "Ty";
+        anotherTiger.breed = "Common tiger, dunno the breed's name LOL";
+        anotherTiger.picture = UIImage(named: "baby.jpeg");
+        
+        var yetAnotherTiger = Tiger();
+        yetAnotherTiger.age = 2;
+        yetAnotherTiger.name = "Tigress";
+        yetAnotherTiger.breed = "3D Digital Animation";
+        yetAnotherTiger.picture = UIImage(named: "tigress_by_jona.jpg");
+        
+        var tigers:Array<Tiger> = [];
+        
+        tigers.append(tiger);
+        tigers.append(anotherTiger);
+        tigers.append(yetAnotherTiger);
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nextBarBtnPressed(sender: AnyObject) {
+    }
+
+}
+
